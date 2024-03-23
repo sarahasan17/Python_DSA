ec02688338e9c9d195ea3166705ff89cbd985a13
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-03-23 21:34:20 +0530

    Update merge_2_sorrted_linked_list.py

--- merge_2_sorrted_linked_list.py
+++ merge_2_sorrted_linked_list.py
@@ -1,40 +1,73 @@
 class listnode:
-    def __init__(self,data):
-        self.data=data
+    def __init__(self,val):
+        self.val=val
         self.next=None
-def insertele(val):
-        t=listnode(0)
-        dummy=t
-        for i in val:
-            dummy.next=listnode(i)
-            dummy=dummy.next
-        return t.next
-            
-def mergeele(l1,l2):
-    t=listnode(0)
-    curr=t
-    while(l1 and l2):
-        if(l1.data<l2.data):
-            curr.next=l1
-            l1=l1.next
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.head1=None
+    def addele1(self,val):
+        temp=listnode(val)
+        if self.head is None:
+            self.head=temp
+            return
         else:
-            curr.next=l2
-            l2=l2.next
-        curr=curr.next
-    if l1:
-        curr.next=l1
-    else:
-        curr.next=l2
-    return t.next
+            t=self.head
+            while(t.next!=None):
+                t=t.next
+            t.next=temp
+    def addele2(self,val):
+        temp=listnode(val)
+        if(self.head1==None):
+            self.head1=temp
+            return
+        else:
+            t=self.head1
+            while(t.next!=None):
+                t=t.next
+            t.next=temp
+    def merge_sorted(self):
+        t1=self.head
+        t2=self.head1
+        t=listnode(0)
+        dump=t
+        while(t1 and t2):
+            if(t1.val<t2.val):
+                dump.next=listnode(t1.val)
+                t1=t1.next
+            else:
+                dump.next=listnode(t2.val)
+                t2=t2.next
+            dump=dump.next
+        if(t1!=None):
+            dump.next=t1
+        else:
+            dump.next=t2
+        return t.next
+l=LinkedList()
+n=int(input("no of elements for linkedlist 1:"))
+print("Enter elements:")
+for i in range(0,n):
+    b=int(input())
+    l.addele1(b)
+m=int(input("no of elements for linkedlist 2:"))
+print("Enter elements:")
+for i in range(0,m):
+    b=int(input())
+    l.addele2(b)
+t=l.merge_sorted()
+while(t!=None):
+    print(t.val,end=' ')
+    t=t.next
 
-l1=[1,2,3,6]
-l2=[4,5,7]
-m=insertele(l1)
-n=insertele(l2)
-merge=mergeele(m,n)
-while merge:
-    print(merge.data,end=' ')
-    merge=merge.next
+
+
+            
+                
+                
+    
+    
+        
     
     
                 
